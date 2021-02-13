65625f53b0a09adb89ed5731890890196ee97c43
BrianMarquez3 <brian3marquez@gmail.com>
2021-02-13 16:48:12 -0500

    feat Condiciones COMPLETE

--- 005 Condicionales/condicionales.js
+++ 005 Condicionales/condicionales.js
@@ -2,10 +2,27 @@
 /* Condicionales */
 /***********************************************/
 
-let num = 5;
+let num1 = 0;
+let num2 = 0;
 
-/* Condicional Simple*/
-if(num > 0){
+/********** Condicional Simple ***********/
+if(num > 0) {
     console.log(`${num} es mayor que 0`);
 }
 
+/********** Condicional Compuesto ***********/
+if(num > 0) {
+    console.log(`${num} es mayor que 0`);
+}else{
+    console.log(`${num} es menor que 0`);
+}
+
+/********** Condicional Multiple ***********/
+if(num > 0) {
+    console.log(`${num} es mayor que 0`);
+
+}else if(num<0){
+    console.log(`${num} es menor que 0`);
+}else{
+    console.log(`${num} es igual a 0`);
+}
