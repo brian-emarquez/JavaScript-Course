bc0f3f55212641b812f9480d21cb1d5fe102a4de
BrianMarquez3 <brian3marquez@gmail.com>
2021-02-13 20:43:05 -0500

    feat Ordenar 3 numeros COMPLETE
    Ordenar 3 numeros COMPLETE
    
    resolves: #
    see also: #

--- ordenar 3 numero de mayor a menor/ordear.js
+++ ordenar 3 numero de mayor a menor/ordear.js
@@ -2,15 +2,15 @@
 /* posibilidades 3! = 3*2*2 = 6
 
 /*
-abc = 123
-acb = 132
-bac = 213
-bca = 231
-cab = 312
-cba = 321
+abc = 321
+acb = 312
+bac = 231
+bca = 132
+cab = 213
+cba = 123
 */
 
-const numbers = document.getElementById('number');
+const numbers = document.getElementById('numbers');
 const result  = document.getElementById('result');
 
 let a=prompt("Introdusca el primer numero")
@@ -18,3 +18,25 @@ let b=prompt("Introdusca el segundo numero")
 let c=prompt("Introdusca el tercer numero")
 
 numbers.textContent = `Los Numeros Introducidos son ${a}, ${b}, ${c}`
+
+if(a>=b && a>=c){ 
+    if(b>c){
+        result.textContent =`El orden es: ${a}, ${b}, ${c}`
+    }else{
+        result.textContent =`El orden es: ${a}, ${c}, ${b}`
+    }
+}else if (b>=a && b>=c){
+    if(a>c){
+    result.textContent =`El orden es: ${b}, ${a}, ${c}`
+    }else{
+        result.textContent =`El orden es: ${b}, ${c}, ${a}`
+    }
+}else if(c>=a && c>=b){
+    if(a>b){
+        result.textContent =`El orden es: ${c}, ${a}, ${b}`
+    }else{
+        result.textContent =`El orden es: ${c}, ${b}, ${a}`
+    }
+}
+
+
