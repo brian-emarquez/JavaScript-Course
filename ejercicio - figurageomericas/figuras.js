bf8c35461d3ff3045918ab3da20732afcebd1e3d
BrianMarquez3 <brian3marquez@gmail.com>
2021-03-13 16:33:31 -0500

    OBJETOS
    OBJETOS
    
    see also: #
    resolves:#

--- ejercicio - figurageomericas/figuras.js
+++ ejercicio - figurageomericas/figuras.js
@@ -1,27 +1,30 @@
 /*Edades*/
 
-let figura = prompt("Introduce la digura geometica que quieres calcular el area: triangulo , rectangulo o curculo")
+let figura = prompt("Introduce la digura geometica que quieres calcular el area: triangulo , rectangulo o circulo")
 
 let base;
 let height;
 let radius;
 
-switsh(figure){
-
+switch(figura){
     case 'triangulo':
-        base = prompt("Introdice la base del triangulo")
+        base = prompt("Introduce la base del triangulo")
         height = prompt("Introduce la altura del triangulo")
         console.log(`El area del triangulo es ${(base*height)/2}`);
         break
 
-    case 'triangulo':
-        base = prompt("Introdice la base del triangulo")
+    case 'rectangulo':
+        base = prompt("Introduce la base del resctangulo")
         height = prompt("Introduce la altura del triangulo")
-        console.log(`El area del triangulo es ${(base*height)/2}`);
+        console.log(`El area del restangulo es ${base*height}`);
         break
-       
 
+    case 'circulo':
+        radius = prompt("Introduce la base del circulo")
+        console.log(`El area del ciruclo es ${Math.PI * Math.pow(radius,2)}`);
+        break
 
-
+    default:
+        console.log('Dato no valido')
 
 }
