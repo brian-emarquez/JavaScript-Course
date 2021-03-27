da32b304df22095476dcdf4481403947d6ca3491
BrianMarquez3 <brian3marquez@gmail.com>
2021-03-27 16:12:17 -0500

    Metodos de lo Arrays

--- 018 Metodo de lo arrays/poo.js
+++ 018 Metodo de lo arrays/poo.js
@@ -32,5 +32,17 @@ console.log(wordSome.some(word => word.length>10));
 console.log(wordSome.every(word => word.length>10)); // todos tiene que cumplir la condicion
 
 /*.map(callback) - Transforma todos los elelentso del array y devuelve un nuevo array*/
-/*.ilter - trnasoforma todos los elementos del array y devuelven un nuevo array*/
+console.log('===============USO DEL .map==================='); // similar a un for
+const numbersMap = [12, 25, 47, 84, 98] // recorrido a un array
+numbersMap.map((numbersMap => console.log(numbersMap*2)));
+
+/*.filter - trnasoforma todos los elementos del array y devuelven un nuevo array*/
+console.log('===============USO DEL .filter===================');
+const numbersFilter = [12, 25, 47, 84, 98]
+const filter = numbersFilter.filter(number => number > 90)
+console.log( filter);
+
 /*.reduce - Reduce todos los elelentos del array a un unico valor */
+console.log('===============USO DEL .reducer===================');
+const numbersReducer = [12, 25, 47, 84, 98]
+console.log(numbersReducer.reduce((a,b) => a+b)); // se optiene la suma
