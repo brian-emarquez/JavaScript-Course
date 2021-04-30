c95c2dad4dc28cef136c012caa6deadaffeaece5
BrianMarquez3 <brian3marquez@gmail.com>
2021-04-30 12:19:23 -0500

    Seleccion de elemento Update

---
+++ 021 Seleccion de Elementos/JavaScript.js
@@ -0,0 +1,17 @@
+
+
+// document.getElementById('id') - Acceder a un elemento a traves de su id
+// const title = document.getElementById('title');
+// /*Console.log(title);*/
+// //permite leer o editar
+// title.textContent = 'Dom Pueba de test'
+
+
+// documents.querySelector('selectorCSS)- ACCEDE AL PRIMER ELELENTO QUE COINCIDA CONgit  EL SELECTOR css
+const paragraph = document.querySelector('.paragraph:nth-child(2)')
+console.log(paragraph)
+
+
+
+// Document.querySelectorAll('selectorCSS`) - accede a todos lo elementos que coinciden con el selector css , devuelve un nodelist
+
