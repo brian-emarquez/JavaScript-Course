38fe5b7615b145319ebef2df8854bd7fa8126b0e
BrianMarquez3 <brian3marquez@gmail.com>
2021-05-07 18:15:47 -0500

    Objento EVENTO

--- 024 Objeto Evento/JavaScript.js
+++ 024 Objeto Evento/JavaScript.js
@@ -1,12 +1,13 @@
 const form = document.getElementById('form')
 const input = document.getElementById('input')
+const button = document.getElementById('button')
 
+// Objeto evento
+// input.addEventListener('keyup', (e) => {
+//     // console.log(input.value);
+//     console.log(e);
+// } )
 
-
-
-
-
-
-
-
-
+button.addEventListener('click', (e)=>{
+    console.log(e.target);
+})
