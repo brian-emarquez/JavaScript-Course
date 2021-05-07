14c93bf9ac88c645be07ab49b6ff38861b6fa721
BrianMarquez3 <brian3marquez@gmail.com>
2021-05-07 17:30:57 -0500

    Objento EVENTO

--- 024 Objeto Evento/JavaScript.js
+++ 024 Objeto Evento/JavaScript.js
@@ -1,26 +1,12 @@
-
-
-const button = document.getElementById('button')
-const box = document.getElementById('box')
+const form = document.getElementById('form')
 const input = document.getElementById('input')
 
-box.addEventListener('mouseenter', ()=>{
-    box.classList.replace('red', 'green')
-})
- box.addEventListener('mouseleave', ()=>{
-     box.classList.replace('green', 'red')
- })
 
- box.addEventListener('mousedown', () =>{
-    console.log('has pulsado en la caja');
- })
 
- box.addEventListener('mouseup', () =>{
-     console.log('HAS SOLTADO EL BOTON IZQUIERDO EN LA CAZA');
- })
 
- input.addEventListener('keydown', ()=>{
-     console.log("Has puldado una tecla")
- })
+
+
+
+
 
 
